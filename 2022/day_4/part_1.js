8a41ac2a69a9da3845398f70a0b350f994f23c95
GaJin Kim <gajin2000@gmail.com>
2022-12-03 23:19:14 -0600

    finish day 4

---
+++ 2022/day_4/part_1.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+
+let ranges = fs.readFileSync("input.txt").toString().split("\n");
+let pairs = 0;
+
+for (let i = 0; i < ranges.length; i++) {
+  both = ranges[i].split(",");
+  left = both[0].split("-");
+  right = both[1].split("-");
+
+  left[0] = parseInt(left[0]);
+  left[1] = parseInt(left[1]);
+  right[0] = parseInt(right[0]);
+  right[1] = parseInt(right[1]);
+
+  console.log(left, right);
+  if (
+    left[0] >= right[0] &&
+    left[0] <= right[1] &&
+    left[1] >= right[0] &&
+    left[1] <= right[1]
+  ) {
+    pairs++;
+  } else if (
+    right[0] >= left[0] &&
+    right[0] <= left[1] &&
+    right[1] >= left[0] &&
+    right[1] <= left[1]
+  ) {
+    pairs++;
+  }
+}
+
+console.log(pairs);

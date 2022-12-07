a8dfca396c0add23091b3734fa6dcf4492b91ec8
GaJin Kim <gajin2000@gmail.com>
2022-12-07 01:32:38 -0600

    redid day 7 the correct way

--- 2022/day_7/part_1.js
+++ 2022/day_7/part_1.js
@@ -1,7 +1,44 @@
+const { dir } = require("console");
 const fs = require("fs");
 
 let foo = fs.readFileSync("input.txt").toString().split("\n");
 
-for (let i = 0; i < foo.length; i++) {
-    console.log(foo[i]);
+let path = [];
+let map = new Map();
+for (let i = 0; i < foo.length - 1; i++) {
+  current = foo[i].split(" ").map((item) => item.trim());
+
+  if (current[1] == "cd") {
+    if (current[2] == "..") {
+      path.pop();
+    } else {
+      path.push(current[2]);
+    }
+  } else if (current[1] == "ls") {
+    continue;
+  } else if (current[0] == "dir") {
+    continue;
+  } else {
+    size = parseInt(current[0]);
+    // console.log(path, current[0]);
+
+    for (let i = 0; i <= path.length; i++) {
+      absPath = "/" + path.slice(0, i);
+      if (map.has(absPath)) {
+        map.set(absPath, map.get(absPath) + size);
+      } else {
+        map.set(absPath, size);
+      }
+    }
+  }
 }
+
+total = 0;
+for (const [key, value] of map.entries()) {
+  if (value <= 100000) {
+    total += value;
+  }
+}
+
+console.log(map);
+console.log(total);
--- 2022/day_7/part_2.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-
-let foo = fs.readFileSync("input.txt").toString().split("\n");
-
-for (let i = 0; i < foo.length; i++) {
-    console.log(foo[i]);
-}

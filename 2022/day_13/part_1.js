32a9f297758031e7d41c604ad6dbd090fcd54759
GaJin Kim <gajin2000@gmail.com>
2022-12-11 22:57:08 -0600

    update skeleton code for day 13

--- 2022/day_13/part_1.js
+++ 2022/day_13/part_1.js
@@ -3,5 +3,6 @@ const fs = require("fs");
 let file = fs.readFileSync("input.txt").toString().split("\n");
 
 for (let i = 0; i < file.length; i++) {
-    console.log(foo[i]);
+    line = file[i];
+    console.log(line);
 }

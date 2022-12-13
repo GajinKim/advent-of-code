061798f56541b69ee593c0e3c8fa7f2bf14a0fd8
GaJin Kim <gajin2000@gmail.com>
2022-12-12 23:48:20 -0600

    finish day 13

--- 2022/day_13/part_1.js
+++ 2022/day_13/part_1.js
@@ -2,7 +2,51 @@ const fs = require("fs");
 
 let file = fs.readFileSync("input.txt").toString().split("\n");
 
-for (let i = 0; i < file.length; i++) {
-    line = file[i];
-    console.log(line);
+let rightOrderCount = [];
+let index = 0;
+
+for (let i = 0; i < file.length; i += 3) {
+  index++;
+  first = eval(file[i]);
+  second = eval(file[i + 1]);
+
+  if (compare(first, second)) {
+    rightOrderCount.push(index);
+  }
 }
+
+function compare(a, b) {
+  if (Number.isInteger(a) && Number.isInteger(b)) {
+    if (a < b) {
+      return true;
+    } else if (a > b) {
+      return false;
+    } else {
+      return undefined; // cycle once
+    }
+  } else if (Array.isArray(a) && Array.isArray(b)) {
+    for (let i = 0; i < a.length && i < b.length; i++) {
+      let comp = compare(a[i], b[i]);
+      if (comp != undefined) {
+        return comp;
+      }
+    }
+    if (a.length < b.length) {
+      return true;
+    } else if (a.length > b.length) {
+      return false;
+    }
+    return undefined;
+  } else {
+    return compare(
+      Number.isInteger(a) ? [a] : a,
+      Number.isInteger(b) ? [b] : b
+    );
+  }
+}
+
+const answer = rightOrderCount.reduce((total, curVal) => {
+  return (total += curVal);
+});
+
+console.log(answer);
--- 2022/day_13/part_2.js
+++ 2022/day_13/part_2.js
@@ -2,7 +2,48 @@ const fs = require("fs");
 
 let file = fs.readFileSync("input.txt").toString().split("\n");
 
-for (let i = 0; i < file.length; i++) {
-    line = file[i];
-    console.log(line);
+let packetOne = [[2]];
+let packetTwo = [[6]];
+const packets = [
+  packetOne,
+  packetTwo,
+  ...file.filter((p) => p !== "").map(eval),
+];
+
+packets.sort((a, b) => {
+  return compare(a, b) ? -1 : 1;
+});
+
+function compare(a, b) {
+  if (Number.isInteger(a) && Number.isInteger(b)) {
+    if (a < b) {
+      return true;
+    } else if (a > b) {
+      return false;
+    } else {
+      return undefined; // cycle once
+    }
+  } else if (Array.isArray(a) && Array.isArray(b)) {
+    for (let i = 0; i < a.length && i < b.length; i++) {
+      let comp = compare(a[i], b[i]);
+      if (comp != undefined) {
+        return comp;
+      }
+    }
+    if (a.length < b.length) {
+      return true;
+    } else if (a.length > b.length) {
+      return false;
+    }
+    return undefined;
+  } else {
+    return compare(
+      Number.isInteger(a) ? [a] : a,
+      Number.isInteger(b) ? [b] : b
+    );
+  }
 }
+
+const answer =
+  (packets.indexOf(packetOne) + 1) * (packets.indexOf(packetTwo) + 1);
+console.log(answer);
